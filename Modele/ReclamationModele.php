9b8fc0ca602c8cc3d5baf7e2a92160fe894ba330
OmarSoltane <omar.soltane@esprit.tn>
2013-05-05 22:12:10 +0300

    Update ReclamationModele.php

--- Modele/ReclamationModele.php
+++ Modele/ReclamationModele.php
@@ -3,100 +3,209 @@
    
 	include "../Control/Configuration.php";
 	
-	class reclamation
+	class reclamation 
 {
-	private $IdReclamation;
-	private $Sujet ;
-	private $Description;
-	private $DateE ;
-	private $IdClient ;
+	private $idReclamation;
+	private $sujet;
+	private $text;
+	private $etatM;
+	private $etatC;
+	private $traitement;
+	private $date_envoi;
+	private $IdCollaborateur;
+	private $IdClient;
 	
+	function __construct($idReclamation , $sujet , $text , $etatM , $etatc , $traitement , $date_envoi,$IdCollaborateur,$IdClient)
 	
-	
-		
-		
-		function __construct($IdReclamation , $Sujet , $Description , $DateE , $IdClient )
-		{
-			$this->IdReclamation = $IdReclamation;
-			$this->Sujet = $Sujet;
-			$this->Description = $Description;
-			$this->DateE = $DateE;
-			$this->IdClient = $IdClient;
-			
-		}
-		
-		
-		function getId_reclamation()
-		{
-			return $this->IdReclametion;
-		}
-		function setId_reclamation($I)
-		{
-			$this->IdReclamation = $I;
-		}
-		
-		function getSujet()
-		{
-			return $this->Sujet;
-		}
-		function setSujet($N)
-		{
-			$this->Sujet=$N;
-		}
-		function getDescription()
-		{
-			return $this->Description;
-		}
-		function setDescription($P)
-		{
-			$this->Description=$P;
-		}
-		function getDateE() {
-			$this->DateE;
-		}
-		function setDateE($N){
-			$this->DateE=$N;
-		}
-		
-		function getIdClient()
-		{
-			return $this->IdClient;
-		}
-		function setIdClient($l)
-		{
-			$this->IdClient=$l;
-		}
-		
-}
-	
-	class ReclamationModele
 	{
-		function __construct ()
-		{
-			$Conf = new Configuration ();
-			$Conf->Connexion ();
-		}
+		$this->idReclamation = $idReclamation;
 		
+		$this->sujet = $sujet;
+		
+		$this->text = $text;
+		
+		$this->etatM = $etatM;
+		
+		$this->etatc = $etatc;
+		
+		$this->traitement = $traitement;
+		
+		$this->date_envoi = $date_envoi;
+		
+		$this->IdCollaborateur = $IdCollaborateur;
+		
+		$this->IdClient = $IdClient;
+	}
+	
+	function __construct( $sujet , $text , $etatM , $etatc , $traitement , $date_envoi,$IdCollaborateur,$IdClient)
+	
+	{
+		$this->sujet = $sujet;
+		$this->text = $text;
+		$this->etatM = $etatM;
+		$this->etatc = $etatc;
+		$this->traitement = $traitement;
+		$this->date_envoi = $date_envoi;
+		$this->IdCollaborateur = $IdCollaborateur;
+		$this->IdClient = $IdClient;
+	}
+	
+	/**
+	 * @return the $idReclamation
+	 */
+	public function getIdReclamation() {
+		return $this->idReclamation;
+	}
+
+/**
+	 * @return the $sujet
+	 */
+	public function getSujet() {
+		return $this->sujet;
+	}
+
+/**
+	 * @return the $text
+	 */
+	public function getText() {
+		return $this->text;
+	}
+
+/**
+	 * @return the $etatM
+	 */
+	public function getEtatM() {
+		return $this->etatM;
+	}
+
+/**
+	 * @return the $etatC
+	 */
+	public function getEtatC() {
+		return $this->etatC;
+	}
+
+/**
+	 * @return the $traitement
+	 */
+	public function getTraitement() {
+		return $this->traitement;
+	}
+
+/**
+	 * @return the $date_envoi
+	 */
+	public function getDate_envoi() {
+		return $this->date_envoi;
+	}
+
+/**
+	 * @return the $IdCollaborateur
+	 */
+	public function getIdCollaborateur() {
+		return $this->IdCollaborateur;
+	}
+
+/**
+	 * @return the $IdClient
+	 */
+	public function getIdClient() {
+		return $this->IdClient;
+	}
+
+/**
+	 * @param field_type $idReclamation
+	 */
+	public function setIdReclamation($idReclamation) {
+		$this->idReclamation = $idReclamation;
+	}
+
+/**
+	 * @param field_type $sujet
+	 */
+	public function setSujet($sujet) {
+		$this->sujet = $sujet;
+	}
+
+/**
+	 * @param field_type $text
+	 */
+	public function setText($text) {
+		$this->text = $text;
+	}
+
+/**
+	 * @param field_type $etatM
+	 */
+	public function setEtatM($etatM) {
+		$this->etatM = $etatM;
+	}
+
+/**
+	 * @param field_type $etatC
+	 */
+	public function setEtatC($etatC) {
+		$this->etatC = $etatC;
+	}
+
+/**
+	 * @param field_type $traitement
+	 */
+	public function setTraitement($traitement) {
+		$this->traitement = $traitement;
+	}
+
+/**
+	 * @param field_type $date_envoi
+	 */
+	public function setDate_envoi($date_envoi) {
+		$this->date_envoi = $date_envoi;
+	}
+
+/**
+	 * @param field_type $IdCollaborateur
+	 */
+	public function setIdCollaborateur($IdCollaborateur) {
+		$this->IdCollaborateur = $IdCollaborateur;
+	}
+
+/**
+	 * @param field_type $IdClient
+	 */
+	public function setIdClient($IdClient) {
+		$this->IdClient = $IdClient;
+	}
+
+	
 		
 		function AjouterReclamation ($R)
 		{			
 			$Requete = "insert into reclamation
 						(
-						IdReclamation,
+						idReclamation,
 					    Sujet ,
-					    Description,
-						DateE,
-						IdClient 
+					    text,
+						etatM,
+						etatC,
+						traitement ,
+						date_envoi,
+						IdCollaborateur,
+						IdClient
+						 
 						
 					    
 													
 						)
 						values
 						(
-							'" .$R->getId_reclamation()."',
+							'" .$R->getIdReclamation()."',
 							'" .$R->getSujet()."',
-							'" .$R->getDescription()."',
-							'" .$R->getDateE()."',
+							'" .$R->getText()."',
+							'" .$R->getEtatM()."',
+							'" .$R->getEtatC()."'
+							'" .$R->getTraitement()."'
+							'" .$R->getDate_envoi()."'
+							'" .$R->getIdCollaborateur()."'
 							'" .$R->getIdClient()."'
 							
 																	
@@ -115,7 +224,7 @@
 		{		
 		
 		
-		$Requete="update  `reclamation` set  `Sujet` =  '".$E->getSujet()."',`Description` =  '".$E->getDescription()."',`DateE` =  '".$E->getDateE()."',`IdClient` =  '".$E->getEIdClient().;
+		$Requete="update  `reclamation` set  `Sujet` =  '".$E->getSujet()."',`text` =  '".$E->getText()."',`etatM` =  '".$E->getEtatM()."',`etatC` =  '".$E->getEtatC(). "',`traitement` =  '".$E->getTraitement()."',`date_envoi` =  '".$E->getDate_envoi()."',`IdCollaborateur` =  '".$E->getIdCollaborateur()."',`IdClient` =  '".$E->getIdClient(). ;
 			
 		
 			mysql_query ($Requete) or die ("Erreur".mysql_error());
@@ -137,11 +246,15 @@
 					
 					
 					
-							 $R = new reclamation (0,"","","","");
-							 $R->setId_reclamation($ligne["IdReclamation"]);		
+							 $R = new reclamation (0,"","","","","","","","");
+							 $R->setIdReclamation($ligne["IdReclamation"]);		
 							 $R->setSujet($ligne["Sujet"]);
-							 $R->setDescription($ligne["Description"]);
-							 $R->setDateE($ligne["DateE"]);
+							 $R->setText($ligne["text"]);
+							 $R->setEtatM($ligne["etatM"]);
+							 $R->setEtatC($ligne["etatC"]);
+							 $R->setTraitement($ligne["traitement"]);
+							 $R->setDate_envoi($ligne["date_envoi"]);
+							 $R->setIdCollaborateur($ligne["IdCollaborateur"]);
 							 $R->setIdClient($ligne["IdClient"]);
 							 
 							
@@ -161,10 +274,5 @@
 	}
 	
 	}
-//	$RM = new CollaborateurModele();
-	//$R=new collaborateur();
-	//$RM->AjouterCollaborateur($R);
-	//$RM->SupprimerCollaborateur(7);
-	//$RM->ModifierCollaborateur($R);
-	//$Liste= $RM->listeCollaborateur();
+
 	?>
